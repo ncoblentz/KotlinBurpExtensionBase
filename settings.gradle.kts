24db57e8490559aec25b8dfcb960bb9b47c7f8e6
Nick Coblentz <nick.coblentz@gmail.com>
2024-09-20 14:06:55 -0500

    Comment added for settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    //https://plugins.gradle.org/plugin/org.gradle.toolchains.foojay-resolver-convention
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "KotlinBurpExtensionBase"
